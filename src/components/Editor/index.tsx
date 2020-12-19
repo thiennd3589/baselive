e019a3f1f0f8219f6fcf288eb347d37f97b3ac2c
thiennd3589 <thien.nd3589@gmail.com>
2020-12-19 21:43:34 +0700

    fix editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -15,6 +15,11 @@ interface EditorCompState {
 }
 
 class EditorContainer extends React.Component<EditorProps, EditorCompState> {
+  private refState = this.props.value
+    ? EditorState.createWithContent(
+        convertFromRaw(JSON.parse(this.props.value))
+      )
+    : EditorState.createEmpty();
   constructor(props: EditorProps) {
     super(props);
     this.state = {
@@ -22,27 +27,17 @@ class EditorContainer extends React.Component<EditorProps, EditorCompState> {
       updateFlag: true,
     };
   }
-
-  componentDidUpdate() {
-    this.props.value &&
-      this.state.updateFlag &&
-      this.setState({
-        editorState: EditorState.createWithContent(
-          convertFromRaw(JSON.parse(this.props.value))
-        ),
-        updateFlag: false,
-      });
-  }
-
-  componentDidMount() {
-    this.props.value &&
-      this.state.updateFlag &&
-      this.setState({
-        editorState: EditorState.createWithContent(
-          convertFromRaw(JSON.parse(this.props.value))
-        ),
-        updateFlag: false,
-      });
+  shouldComponentUpdate(nextProps: EditorProps) {
+    console.log(this.props, nextProps);
+    if (this.props.value !== nextProps.value) {
+      nextProps.value
+        ? (this.refState = EditorState.createWithContent(
+            convertFromRaw(JSON.parse(nextProps.value))
+          ))
+        : (this.refState = EditorState.createEmpty());
+      return true;
+    }
+    return false;
   }
 
   onEditorStateChange = (editorState: EditorState) => {
@@ -65,7 +60,7 @@ class EditorContainer extends React.Component<EditorProps, EditorCompState> {
       <div className={this.props.readonly ? "editor readonly" : "editor"}>
         <Editor
           readOnly={this.props.readonly}
-          editorState={editorState}
+          editorState={this.props.readonly ? this.refState : editorState}
           onEditorStateChange={this.onEditorStateChange}
           toolbar={{
             inline: { inDropdown: true },
